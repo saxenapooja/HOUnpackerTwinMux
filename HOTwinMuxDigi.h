1d9a77e43334b1e5bed7bb609db3a7ae628bb379
saxenapooja <pooja.saxeba@cern.ch>
2016-12-14 13:43:22 +0100

    tested version, modified the structure and fixed some bugs

--- HOTwinMuxDigi.h
+++ HOTwinMuxDigi.h
@@ -18,54 +18,65 @@ class HOTwinMuxDigi {
 
   HOTwinMuxDigi() {theTP_HO=0;}
   HOTwinMuxDigi(uint64_t data) {theTP_HO = data;}
-  HOTwinMuxDigi(int ieta, int iphi, int mip, int sector, int wheel, int index, int link, bool validbit );
+  HOTwinMuxDigi(int ieta, int iphi, int bx, int mip, int validbit, int wheel, int sector, int index, int link);
+  // HOTwinMuxDigi(int ieta, int iphi, int mip, bool validbit, signed int BX); //better official alternative
 
   const HcalDetId id() const { return HcalDetId(HcalOuter,ieta(),iphi(),4); }
-
+  
   /// get raw packed HO 
   uint64_t raw() const {return theTP_HO; }
-
+  
   /// get the raw ieta value
   int raw_ieta() const {return theTP_HO&0x1F; }
-
+  
   /// get the sign of ieta (int: +/- 1)
   int ieta_sign() const {return ( (theTP_HO&0x10)?(-1):(+1)); }
 
   /// get the absolute value of ieta
   int ieta_abs() const {return (theTP_HO&0x000F); }
-
+  
   /// get the signed ieta value
   int ieta() const {return (ieta_abs() * ieta_sign()); }
 
   /// get the raw iphi value
   int iphi() const {return (theTP_HO>>5)&0x007F; }
+  
+  /// get the bx()
+  int bx_abs() const {return (theTP_HO>>12)&0x1; }
 
+  /// get the bx sign
+  //  int bx_sign() const {return ( ( (theTP_HO>>13)&0x2000) ?(-1):(+1)); }
+  int bx_sign() const {return ( ( (theTP_HO>>13)&0x1) ?(-1):(+1)); }
+
+  //get bx
+  int bx() const {return  (bx_abs() * bx_sign()); } 
+  
   /// get the mip value
-  int mip() const {return (theTP_HO>>16)&0x1; }
-
-  /// get the sector value
-  int sector() const {return (theTP_HO>>17)&0xF; }
-  
-  /// get the raw wheel value
-  int raw_wheel() const {return (theTP_HO>>21)&0x7; }
-
-  /// get the sign of wheel (int: +/- 1)                                                                                                                                   
-  int wheel_sign() const {return ( ( (theTP_HO>>23)&0x10) ?(-1):(+1)); }
-
-  /// get the absolute value of wheel                                                                                                                                       
-  int wheel_abs() const {return (theTP_HO>>21)&0x03; }
-  
-  /// get the signed wheel value                                                                                                                                          
-  int wheel() const {return (wheel_abs() * wheel_sign()); }
-
-  /// get the index
-  int index() const {return (theTP_HO>>24)&0x3F; }
-
-  /// get the link value
-  int link() const {return (theTP_HO>>30)&0x3; }
+  int mip() const {return (theTP_HO>>14)&0x1; }
 
   /// get the valid bit
-  int validbit() const {return (theTP_HO>>32)&0x1; }
+  int validbit() const {return (theTP_HO>>15)&0x1; }
+
+  /// get the raw wheel value
+  int raw_wheel() const {return (theTP_HO>>16)&0x7; }
+  
+  /// get the sign of wheel (int: +/- 1)             
+  int wheel_sign() const {return ( ( (theTP_HO>>18)&0x1) ?(-1):(+1)); }
+
+  /// get the absolute value of wheel                                             
+  int wheel_abs() const {return (theTP_HO>>16)&0x03; }
+  
+  /// get the signed wheel value                                                
+  int wheel() const {return (wheel_abs() * wheel_sign()); }
+
+  /// get the sector value
+  int sector() const {return (theTP_HO>>19)&0xF; }
+  
+  /// get the index
+  int index() const {return (theTP_HO>>23)&0x1F; }
+
+  /// get the link value
+  int link() const {return (theTP_HO>>28)&0x3; }
 
   static const int HO_SECTOR_MAX = 12;
 
