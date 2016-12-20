96800e6c1f439287f3ff2cda6bb41fc639f1c03a
saxenapooja <pooja.saxeba@cern.ch>
2016-12-20 09:58:24 +0100

    created collection of HOTPs in vector

--- HOTPUnpacker.h
+++ HOTPUnpacker.h
@@ -1,46 +1,44 @@
 /* -*- C++ -*- */
-#ifndef HcalUnpacker_h_included
-#define HcalUnpacker_h_included 1
+#ifndef HOTPUnpacker_h_included
+#define HOTPUnpacker_h_included 1
 
+#include "DataFormats/HcalDigi/interface/HcalCalibDataFrame.h"
 #include "DataFormats/HcalDigi/interface/HOTriggerPrimitiveDigi.h"
-#include "DataFormats/HcalDigi/interface/HcalUnpackerReport.h"
 #include "DataFormats/FEDRawData/interface/FEDRawData.h"
 #include "CondFormats/HcalObjects/interface/HcalElectronicsMap.h"
-#include "DataFormats/HcalDigi/interface/HcalTriggerPrimitiveDigi.h"
-#include "DataFormats/HcalDigi/interface/HcalTTPDigi.h"
+#include "DataFormats/HcalDigi/interface/HcalTriggerPrimitiveSample.h"
+#include "DataFormats/HcalDigi/interface/HcalUnpackerReport.h"
 #include <set>
 
 class HOTPUnpacker {
 public:
 
-  struct Collections {
-    Collections();
-    std::vector<HOTriggerPrimitiveDigi>* tphoCont;
+  struct HOCol {
+    HOCol();
+    std::vector<int>*     etaCont;
+    std::vector<int>*     phiCont;
+    std::vector<int>*   sampleCont;
+    std::vector<int>*      soiCont;
+    std::vector<int>* databitsCont;
+    
   };
-
+  
   /// for normal data
   HOTPUnpacker(int sourceIdOffset, int beg, int end) : sourceIdOffset_(sourceIdOffset), startSample_(beg), endSample_(end), expectedOrbitMessageTime_(-1), mode_(0) { }
-
+  /// For histograms, no begin and end
+  
+  HOTPUnpacker(int sourceIdOffset) : sourceIdOffset_(sourceIdOffset), startSample_(-1), endSample_(-1),  expectedOrbitMessageTime_(-1), mode_(0) { }
   void setExpectedOrbitMessageTime(int time) { expectedOrbitMessageTime_=time; }
-
-  struct HOUnrolledTP;
-
-  void unpack(const FEDRawData& raw, 
-	      const HcalElectronicsMap& emap, 
-	      Collections& colls,
-	      HcalUnpackerReport& report, 
-	      bool silent=false);
-
-
+  void unpack(const FEDRawData& raw, const HcalElectronicsMap& emap, HOCol& conts, HcalUnpackerReport& report, bool silent=false);
   void setMode(int mode) { mode_=mode; }
-
+  
 private:
-  int sourceIdOffset_;           ///< number to subtract from the source id to get the dcc id
-  int startSample_;              ///< first sample from fed raw data to copy 
-  int endSample_;                ///< last sample from fed raw data to copy (if present)
+  int sourceIdOffset_; ///< number to subtract from the source id to get the dcc id
+  int startSample_; ///< first sample from fed raw data to copy 
+  int endSample_; ///< last sample from fed raw data to copy (if present)
   int expectedOrbitMessageTime_; ///< Expected orbit bunch time (needed to evaluate time differences)
   int mode_;
   std::set<HcalElectronicsId> unknownIds_,unknownIdsTrig_; ///< Recorded to limit number of times a log message is generated
 };
 
-#endif // HcalUnpacker_h_included
+#endif // HOTPUnpacker_h_included
